f2a9050b56cd3d2f3440c1f7656fc3f2095184c2
Santiago Guzman <superteso567@hotmail.com>
2025-09-02 09:09:25 -0500

    adding first steep

---
+++ 02-firts-steps/src/FirstSteepsApp.tsx
@@ -0,0 +1,12 @@
+export function FirstSteepsApp() {
+    return (
+        <>
+            <h1>Hola Mundo !!</h1>
+            <p>Es un parrafo!</p>
+            <button>Click me</button>
+            <div>
+                <h2>Hola dentro de un div</h2>
+            </div>
+        </>
+    )
+}
