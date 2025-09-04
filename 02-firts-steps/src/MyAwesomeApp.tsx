6ffef3c6178bbdf5da5f14691d394a3a583df823
Santiago Guzman <superteso567@hotmail.com>
2025-09-04 13:34:54 -0500

    add type in reatc

--- 02-firts-steps/src/MyAwesomeApp.tsx
+++ 02-firts-steps/src/MyAwesomeApp.tsx
@@ -1,9 +1,25 @@
 export function MyAwesomeApp() {
+    const firtsName = "Santiago" , lastName = 'Guzman'
 
+    const favoriteGames = ['elden ring' , 'Halo', 'The last of us']
+    const isActive = true;
+
+    const address = {
+        zipCode : '123',
+        country : 'Colombia'
+    }
     return (
         <>
-            <h1>Santiago</h1>
-            <h3>Guzman</h3>
+            <h1> { firtsName } </h1>
+            <h3>{ lastName }</h3>
+
+            <p>
+                { favoriteGames.join(', ') }
+            </p>
+            <p>{ 2 + 2}</p>
+
+            <h1>{ isActive && 'Activo' }</h1>
+            { JSON.stringify(address)  }
         </>
     )
 }
