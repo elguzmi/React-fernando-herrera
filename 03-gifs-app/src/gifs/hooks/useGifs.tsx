c07889122c0d19ec77fa290dd8a7150d52727f27
Cesar Santiago Guzman Prada <santiago.guzman@parceiro.niw.pt>
2025-11-26 14:21:37 -0500

    finish section 2 , use ref

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getGifsByQueryAction } from "../actions/get-gifs-by-query.action";
 import type { Gif } from "../interfaces/gif.interface";
+
+// esta variable se dejo por fuera del componente 
+// ya que al cambiar el estado el componente se vuelve a renderizar y se pierde el estado de la variable
+//const gifCache:Record<string , Gif[]> = {}
 
 export const useGifs = ()=>{
 
     const [ gifs , setGifs ] = useState<Gif[]>([])
     const [ previousSearches, setPreviousSearches ] = useState<string[]>([]) 
+    const gifCache = useRef<Record<string , Gif[]>>({}) // No causa renders
+
 
     const handledSearchClick = async (term:string)=>{
+        console.log(gifCache.current)
+        if(gifCache.current[term]){
+            setGifs(gifCache.current[term])
+            return
+        }
+        
         const gifs = await getGifsByQueryAction(term)
         setGifs(gifs)
     } 
@@ -19,12 +31,12 @@ export const useGifs = ()=>{
         if(previousSearches.includes(searchWord))return
 
         // previousSearches.unshift(searchWord)   // inserta el elemento al inicio del array
-        setPreviousSearches( [ searchWord , ...previousSearches.filter((item,index)=>index<=6)])
+        setPreviousSearches( [ searchWord , ...previousSearches.filter((item,index)=>index<=6 && item)])
         //setPreviousSearches( [ searchWord , ...previousSearches].splice(0,8))
 
         const gifs = await getGifsByQueryAction(searchWord)
-        setGifs(gifs)
-        
+        setGifs(gifs);
+        gifCache.current[query] = gifs;       
     }
 
     return {
