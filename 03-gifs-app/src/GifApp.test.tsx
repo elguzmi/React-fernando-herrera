414ed10f42781fed35c6f1f2dd6aad7db8a11c09
Cesar Santiago Guzman Prada <santiago.guzman@parceiro.niw.pt>
2025-11-26 14:39:30 -0500

    adding testing library and gifapp test

---
+++ 03-gifs-app/src/GifApp.test.tsx
@@ -0,0 +1,10 @@
+import { describe, test, expect } from 'vitest'
+import { render } from '@testing-library/react'
+import { GifsApp } from './GifsApp'
+
+describe('GifApp', ()=>{
+  test('should render component properly', ()=>{
+    const {container} = render(<GifsApp />)
+    expect(container).toMatchSnapshot();
+  })
+})
