dc43acf5e65659597b77b4ca15a5e0fd0d859885
Santiago Guzman <santiago_guzmanprada@hotmail.com>
2025-11-30 22:09:12 -0500

    optimice tests

--- 03-gifs-app/src/shared/components/CustomHeader.test.tsx
+++ 03-gifs-app/src/shared/components/CustomHeader.test.tsx
@@ -1,25 +1,25 @@
 import { describe, expect, test } from "vitest";
 import {CustomHeader} from '../../shared/components/CustomHeader'
-import { render } from "@testing-library/react";
+import { render , screen  } from "@testing-library/react";
 
 describe("CustomHeader", ()=>{  
     const title = 'Titulo de prueba', desciption = 'descripcion de prueba';
 
     test("should render the title correctly", async ()=>{
-        const {findByRole } = render(<CustomHeader  title={title} />)
-        const h1Node =  await findByRole('heading',{level:1})
-        console.log(h1Node.textContent)
+        render(<CustomHeader  title={title} />)
+        const h1Node = await screen.findByRole('heading',{level:1})
         expect(h1Node.textContent).toBe(title)
     });
 
     test('should render the description when provided',async ()=>{
-        const { findByRole } =  render(<CustomHeader  title={title}  description={desciption} />)
-        const pElement =  await findByRole('paragraph')
-        expect(pElement.textContent).toBe(desciption)
+        render(<CustomHeader  title={title}  description={desciption} />)
+        expect(screen.getByRole('paragraph')).toBeDefined();
+        expect(screen.getByRole('paragraph').textContent).toBe(desciption)
     });
 
     test('should not render description when not provided',async ()=>{
         const { container} =  render(<CustomHeader  title={title}  />)
+        // Cuando se usa el screen no puede encontrar el p porque no esta renderizado, por eso podemo extraer el container 
         expect(container.getElementsByTagName('p').length).toBe(0)
     })
 })
